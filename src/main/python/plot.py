f0016deaefe945bedfe092a5f7fdc315d3f2bc9b
Andreas <ahinderyckx@yahoo.co.uk>
2022-04-09 17:33:58 +0200

    Rework reducer logic with subsets and TaxiIDs, ...
    - Rewrote reducer logic:
    	- First check whether TaxiIDs match
    	- Then whether the current record is a subset of the previous
    - Remove ununsed code
    - Added comments explaining the process

--- src/main/python/plot.py
+++ src/main/python/plot.py
@@ -1,7 +1,23 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-df = pd.read_csv("~/Desktop/part-r-00000", delim_whitespace=True, parse_dates=True, names=["Date", "Revenue"])
-df.plot(kind="bar", x='Date', y='Revenue')
-# plt.xticks(rotation=45, ha='right')
+# df = pd.read_csv("~/Desktop/revenue per month wo checking subsequent segments", delim_whitespace=True, parse_dates=True, names=["Date", "Revenue"])
+# df.plot(kind="bar", x='Date', y='Revenue')
+# # plt.xticks(rotation=45, ha='right')
+# plt.show()
+
+execution_times = {
+    9: "9m17.379s",
+    11: "9m53.635s",
+    13: "9m26.603s",
+    15: "9m24.209s",
+    17: "9m22.817s"
+}
+for key, time in execution_times.items():
+    execution_times[key] = float(time[0]) * 60 + float(time[2:4]) + float(time[5:-1]) / 1000
+ordered_times = dict(sorted(execution_times.items()))
+
+plt.plot(ordered_times.keys(), ordered_times.values())
 plt.show()
+
+
