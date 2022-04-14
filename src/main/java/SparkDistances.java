982cd33ab7bd0fecfad80b5fe3d7e19c16ad319e
Andreas <ahinderyckx@yahoo.co.uk>
2022-04-14 22:59:29 +0200

    Add some documentation, remove run.sh

--- src/main/java/SparkDistances.java
+++ src/main/java/SparkDistances.java
@@ -2,24 +2,38 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.sql.*;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A class to calculate the distances of .trip files
+ * based on a Spark implementation
+ */
 public class SparkDistances {
     private final SparkSession spark;
-    private SparkConf conf;
     private final JavaSparkContext jsc;
-    private JavaRDD<Row> data;
 
+    /**
+     * Creates a new SparkDistances instance
+     * with "local" as master, to execute
+     * locally
+     */
     SparkDistances() {
         this("local");
     }
 
+    /**
+     * Initialises a {@link SparkConf}, {@link JavaSparkContext}
+     * and a {@link SparkSession} to be used to calculate
+     * the distances using Spark.
+     *
+     * @param master: the master of this {@link SparkConf}
+     */
     private SparkDistances(String master) {
         SparkConf conf = new SparkConf().setMaster(master).setAppName("TripDistances");
         jsc = new JavaSparkContext(conf);
@@ -30,6 +44,12 @@ public class SparkDistances {
                 .getOrCreate();
     }
 
+    /**
+     * Calculate the distances of the trips in a .trips file
+     * using a Spark transformation
+     *
+     * @throws IOException: If the .trips data can't be read in
+     */
     protected void calculateDistances() throws IOException {
         JavaRDD<Row> data = readData(System.getProperty("user.dir") + "/data/2010_03.trips");
         long currentTime = System.currentTimeMillis();
@@ -39,6 +59,13 @@ public class SparkDistances {
         jsc.stop();
     }
 
+    /**
+     * Write the calculated distances to an output file
+     *
+     * @param distances: the calculated distances to write to a file
+     * @throws IOException: when the file can't be opened
+     *  or the distances can't be written to a file
+     */
     private void writeResults(List<Double> distances) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(System.getProperty("user.dir") + "/data/sparkDistances.csv"));
         for (double d: distances) {
@@ -48,6 +75,11 @@ public class SparkDistances {
         writer.close();
     }
 
+    /**
+     * Read the .trips file to a {@link JavaRDD<Row>} by parallelize.
+     * @param path: the path to the data to be read in
+     * @return rdd: a {@link JavaRDD<Row>} object
+     */
     private JavaRDD<Row> readData(String path) {
         return jsc.parallelize(spark.read()
                 .option("delimiter", " ")
@@ -55,6 +87,14 @@ public class SparkDistances {
                 .csv(path).collectAsList());
     }
 
+    /**
+     * Calculate the distances of the data in a given {@link JavaRDD<Row>} using
+     * a flat surface earth formula. The formula is applied to the data using
+     * a Spark transformation.
+     *
+     * @param data: the data of which the distances are to be calculated
+     * @return distances: a list containing the calculated distances
+     */
     private static List<Double> getDistances(JavaRDD<Row> data) {
         JavaRDD<Double> mapping = data.map((Function<Row, Double>) row -> {
                     try {
@@ -69,7 +109,6 @@ public class SparkDistances {
                     }
                 }
         );
-
         return mapping.collect();
     }
 }
