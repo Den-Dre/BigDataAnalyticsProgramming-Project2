0c71479d478ed37de917027203e9072c64b623a2
Andreas Hinderyckx <andreas.hinderyckx@student.kuleuven.be>
2022-04-14 23:20:52 +0200

    Make runSparkTrips.sh work (uses too much memory)

--- src/main/java/runSparkTrips.sh
+++ src/main/java/runSparkTrips.sh
@@ -1,3 +1,3 @@
-/usr/lib/jvm/java-8-openjdk-amd64/bin/javac -cp ".:$(yarn classpath):/cw/bdap/software/spark-3.2.1-bin-hadoop3.2/{jars,bin,sbin,yarn}/*" Main.java
-/usr/lib/jvm/java-8-openjdk-amd64/bin/jar cmvf META-INF/MANIFEST.MF Main.jar GPSUtil.class Main.class SparkDistances.class 
+/usr/lib/jvm/java-8-openjdk-amd64/bin/javac -cp ".:$(yarn classpath):/cw/bdap/software/spark-3.2.1-bin-hadoop3.2/jars/*" Main.java
+#/usr/lib/jvm/java-8-openjdk-amd64/bin/jar cmvf META-INF/MANIFEST.MF Main.jar GPSUtil.class Main.class SparkDistances.class 
 java -cp "/cw/bdap/software/spark-3.2.1-bin-hadoop3.2/jars/*:." Main
